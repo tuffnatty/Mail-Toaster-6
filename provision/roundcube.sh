f9aa092bd17b1f9f310315a57a02aef80f9ee3e5
Phil Krylov <phil.krylov@gmail.com>
2022-11-27 22:41:10 +0100

    roundcube: Use preconfigured MySQL password

--- provision/roundcube.sh
+++ provision/roundcube.sh
@@ -37,7 +37,7 @@ install_roundcube_mysql()
 			echo "preserving roundcube password $_rcpass"
 		fi
 	else
-		_rcpass=$(get_random_pass 18 safe)
+		_rcpass="$ROUNDCUBE_MYSQL_PASS"
 	fi
 
 	local _rcc_dir="$STAGE_MNT/usr/local/www/roundcube/config"
