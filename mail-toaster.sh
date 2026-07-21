85283f2cf28514199761846571d5fc1f2c533b20
Phil Krylov <phil.krylov@gmail.com>
2026-07-22 00:49:21 +0200

    [feature] mt: added PKG_REPO_{NAME,URL} for custom pkg repo; disable kmods repo in jails

--- mail-toaster.sh
+++ mail-toaster.sh
@@ -394,6 +394,8 @@ start_staged_jail()
 		devfs_ruleset="$JAIL_DEVFS_RULESET" \
 		$JAIL_START_EXTRA
 
+	[ "$_name" = base ] || \
+	[ "$_name" = bsd_cache ] || \
 	enable_bsd_cache
 
 	tell_status "updating pkg database"
